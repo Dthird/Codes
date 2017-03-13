dc8d353fe5691275a7482af37d5e33727a83ccf5
Jiaxing Dong <sydongjx@163.com>
2017-03-13 17:11:51 +0800

    leetcode 66

---
+++ src/leetcode/E66.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+/**
+ * Created by jason on 3/13/17.
+ */
+public class E66 {
+    public int[] plusOne(int[] digits) {
+
+        int n = digits.length;
+        for(int i=n-1; i>=0; i--) {
+            if(digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+
+            digits[i] = 0;
+        }
+
+        int[] newNumber = new int [n+1];
+        newNumber[0] = 1;
+
+        return newNumber;
+    }
+
+}
